e8c0a2a8ee118ba09059caef03c7cf50965d0023
Maryam Fatimah <maryamfatimah4@gmail.com>
2025-12-09 12:31:12 +0700

    Update Tailwind config + rebuild production assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
         "./resources/views/**/*.blade.php",
         "./resources/js/**/*.js",
 
-        // Untuk Filament Admin
+        // Filament admin panel
         "./app/Filament/**/*.php",
         "./resources/views/filament/**/*.blade.php",
         "./vendor/filament/**/*.blade.php",
