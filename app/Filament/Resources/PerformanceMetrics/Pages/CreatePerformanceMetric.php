2b8bb64f943b51402d456853b1e61ac645b42499
Maryam Fatimah <maryamfatimah4@gmail.com>
2025-12-30 13:10:39 +0700

    Fix PerformanceMetric creation logic

--- app/Filament/Resources/PerformanceMetrics/Pages/CreatePerformanceMetric.php
+++ app/Filament/Resources/PerformanceMetrics/Pages/CreatePerformanceMetric.php
@@ -3,32 +3,13 @@
 namespace App\Filament\Resources\PerformanceMetrics\Pages;
 
 use App\Filament\Resources\PerformanceMetrics\PerformanceMetricResource;
-use App\Models\PerformanceMetric;
-use Filament\Resources\Pages\CreateRecord;
 use App\Filament\Resources\BaseCreateRecord;
-
 
 class CreatePerformanceMetric extends BaseCreateRecord
 {
     protected static string $resource = PerformanceMetricResource::class;
 
-    // (optional) kalau nggak mau ada "Create & create another"
-    // protected static bool $canCreateAnother = false;
-
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        $last = PerformanceMetric::query()
-            ->orderBy('metric_id', 'desc')
-            ->first();
-
-        $nextNumber = $last
-            ? ((int) substr($last->metric_id, 3)) + 1
-            : 1;
-
-        $data['metric_id'] = 'MET' . str_pad($nextNumber, 4, '0', STR_PAD_LEFT);
-
-        return $data;
-    }
+    // HAPUS SEMUA mutateFormDataBeforeCreate yang bikin metric_id
 
     public function getTitle(): string
     {
